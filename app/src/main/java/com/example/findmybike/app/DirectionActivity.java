aec00256981bc7bf17a2b2054844fe0332f6f931
Mergim Rama <mergim.1992@gmail.com>
2014-05-13 02:01:48 +0200

    Changed the color of the actionbar and removed the icon. Added a button on the map for directions and created an new activity for it. On the activity I also added an arrow atm just to se how it looks. Next thing is to implement the compass functionality on the DirectionActivity

---
+++ app/src/main/java/com/example/findmybike/app/DirectionActivity.java
@@ -0,0 +1,22 @@
+package com.example.findmybike.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by Mergim on 2014-05-13.
+ */
+public class DirectionActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_directions);
+
+
+    }
+
+
+
+}
