610025d54f4b7cef1e2cf0c48e3cc13b690e8b79
Mergim Rama <mergim.1992@gmail.com>
2014-05-14 16:55:19 +0200

    added logview and a button for further logging code

--- app/src/main/java/com/example/findmybike/app/DirectionActivity.java
+++ app/src/main/java/com/example/findmybike/app/DirectionActivity.java
@@ -6,7 +6,6 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
